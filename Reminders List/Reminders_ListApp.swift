1139afc332c55a82fe631e0343faa1eeb6845be5
Tim Myers <tim@jvsw.com>
2024-12-27 13:12:00 -0500

    Initial Commit

---
+++ Reminders List/Reminders_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reminders_ListApp.swift
+//  Reminders List
+//
+//  Created by Timothy Myers on 12/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Reminders_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Reminders ListTests/Reminders_ListTests.swift
@@ -0,0 +1,16 @@
+//
+//  Reminders_ListTests.swift
+//  Reminders ListTests
+//
+//  Created by Timothy Myers on 12/27/24.
+//
+
+import Testing
+
+struct Reminders_ListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
